d6e3eb6b30edb6776d2691775d435418afc411b2
Anshikabhardwaj-lang <anshikabhardwaj295@gmail.com>
2025-07-04 17:52:08 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	char op;
+	cout<<"enter first number:";
+	cin>>a;
+	cout<<"enter second number:";
+	cin>>b;
+	cout<<"enter operator:";
+	cin>>op;
+	switch(op){
+		case('+'):
+			cout<<"result is"<<a+b;
+			break;
+				case('-'):
+			cout<<"result is"<<a-b;
+			break;
+		     	case('*'):
+			cout<<"result is"<<a*b;
+			break;
+			   case('/'):
+			cout<<"result is"<<a/b;
+			break;
+	
+}
+}
